0102768748d697f53a8198449a1d6fe3d5038d63
Hakan Sayar <143936987+HakanSayar33@users.noreply.github.com>
2023-12-31 00:11:13 +0300

    Add files via upload

---
+++ Magaza_Urun_Takip/Islemler.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Magaza_Urun_Takip
+{
+    partial class Islemler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbMusteri = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPersonel = new System.Windows.Forms.ComboBox();
+            this.cmbUrunler = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.SeaGreen;
+            this.groupBox1.Controls.Add(this.cmbMusteri);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtFiyat);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbPersonel);
+            this.groupBox1.Controls.Add(this.cmbUrunler);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(656, 294);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Satış Yap";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // cmbMusteri
+            // 
+            this.cmbMusteri.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbMusteri.FormattingEnabled = true;
+            this.cmbMusteri.Location = new System.Drawing.Point(231, 129);
+            this.cmbMusteri.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbMusteri.Name = "cmbMusteri";
+            this.cmbMusteri.Size = new System.Drawing.Size(160, 27);
+            this.cmbMusteri.TabIndex = 10;
+            this.cmbMusteri.SelectedIndexChanged += new System.EventHandler(this.cmbMusteri_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(169, 192);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Fiyat:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtFiyat.Location = new System.Drawing.Point(231, 192);
+            this.txtFiyat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(160, 27);
+            this.txtFiyat.TabIndex = 8;
+            this.txtFiyat.TextChanged += new System.EventHandler(this.txtFiyat_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(453, 78);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 80);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Satış Listesini Gör";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(231, 244);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "KAYDET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(151, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Müşteri:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(136, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Personel:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(171, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ürün:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cmbPersonel
+            // 
+            this.cmbPersonel.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbPersonel.FormattingEnabled = true;
+            this.cmbPersonel.Location = new System.Drawing.Point(231, 81);
+            this.cmbPersonel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbPersonel.Name = "cmbPersonel";
+            this.cmbPersonel.Size = new System.Drawing.Size(160, 27);
+            this.cmbPersonel.TabIndex = 1;
+            this.cmbPersonel.SelectedIndexChanged += new System.EventHandler(this.cmbPersonel_SelectedIndexChanged);
+            // 
+            // cmbUrunler
+            // 
+            this.cmbUrunler.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbUrunler.FormattingEnabled = true;
+            this.cmbUrunler.Location = new System.Drawing.Point(231, 26);
+            this.cmbUrunler.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbUrunler.Name = "cmbUrunler";
+            this.cmbUrunler.Size = new System.Drawing.Size(160, 27);
+            this.cmbUrunler.TabIndex = 0;
+            this.cmbUrunler.SelectedIndexChanged += new System.EventHandler(this.cmbUrunler_SelectedIndexChanged);
+            // 
+            // Islemler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(688, 324);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Islemler";
+            this.Text = "Islemler";
+            this.Load += new System.EventHandler(this.Islemler_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPersonel;
+        private System.Windows.Forms.ComboBox cmbUrunler;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.ComboBox cmbMusteri;
+    }
+}
---
+++ Magaza_Urun_Takip/Islemler.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Magaza_Urun_Takip
+{
+    public partial class Islemler : Form
+    {
+        public Islemler()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=.;Initial Catalog=MagazaUrunTakip;Integrated Security=True");
+        private void Islemler_Load(object sender, EventArgs e)
+        {
+            
+            SqlCommand komut = new SqlCommand("select * from TBLURUNLER", baglanti);
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cmbUrunler.ValueMember = "ID";
+            cmbUrunler.DisplayMember = "AD";
+            cmbUrunler.DataSource = dt;
+
+            
+            SqlCommand komut2 = new SqlCommand("select * from TBLMUSTERILER", baglanti);
+            SqlDataAdapter da2 = new SqlDataAdapter(komut2);
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            cmbMusteri.ValueMember = "ID";
+            cmbMusteri.DisplayMember = "ADSOYAD";
+            cmbMusteri.DataSource = dt2;
+
+            
+            SqlCommand komut3 = new SqlCommand("select * from TBLPERSONELLER", baglanti);
+            SqlDataAdapter da3 = new SqlDataAdapter(komut3);
+            DataTable dt3 = new DataTable();
+            da3.Fill(dt3);
+            cmbPersonel.ValueMember = "ID";
+            cmbPersonel.DisplayMember = "AD";
+            cmbPersonel.DataSource = dt3;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Satis_Listesi fr = new Satis_Listesi();
+            fr.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into TBLHAREKETLER(URUN, MUSTERI, PERSONEL, FIYAT) values (@p1,@p2,@p3,@p4)", baglanti);
+            komut.Parameters.AddWithValue("@p1", cmbUrunler.SelectedValue);
+            komut.Parameters.AddWithValue("@p2", cmbMusteri.SelectedValue);
+            komut.Parameters.AddWithValue("@p3", cmbPersonel.SelectedValue);
+            komut.Parameters.AddWithValue("@p4", txtFiyat.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Satış kaydı oluşturuldu");
+            
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbMusteri_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFiyat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbPersonel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbUrunler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
